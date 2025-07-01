4c0573ecae5bf25fa96ddcdf4d136ad768f01b76
1329728 <S01329728@acad.tri-c.edu>
2025-07-01 23:13:44 +0000

    Added Lab2 and Lab3

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d82acc3574d6327b4dd70693ba2cf84945809d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08462b8471f36db10360dc9723965788f2f469dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

029e3dfe9e0f6ee346f554d35b0a5fe77934a842
1329728 <S01329728@acad.tri-c.edu>
2025-07-03 00:04:08 +0000

    Lab1 Complete

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Lab1;
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Step 1
+        /*Kyle Kostansek IT 1050 Lab 1
+        Step 2 */
+        Console.WriteLine(KyleKostansek IT 1050);
 
-    }
+        //Step 3//
+        int myNum = 23;
+        Console.WriteLine("My favorite number is " + myNum + "");
+        string myName = "HTML";
+        Console.WriteLine("My favorite programming language is " + myName + "");
+        double MyDoublenum = 0;
+        Console.WriteLine("I have written " + MyDoublenum + "");
+        bool myBool = true;
+        Console.WriteLine("My programming experience is " + myBool + "");
+
+        //Step 4//
+        const string myName2 = "Tri-C";
+        Console.WriteLine("My school name is " + myName2 + "");
+
+        //Step 5//
+        int myInt = (int)9.78;
+        double doubleValue = 4.89;
+        bool myBool2 = true;
+        Console.WriteLine(Convert.ToString(myInt));
+        Console.WriteLine(Convert.ToString(myBool2));
+        Console.WriteLine("double:" + doubleValue);
+
+        //Step 6//
+        Console.WriteLine("Enter Name");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Enter Age");
+        string Age = Console.ReadLine();
+        Convert.ToInt32();
+        Console.WriteLine("Your name and age are: " + Name + Age);
+
+    
+
+
+        
+
+
+
+    }   
 }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08462b8471f36db10360dc9723965788f2f469dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0573ecae5bf25fa96ddcdf4d136ad768f01b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

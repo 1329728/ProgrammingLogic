f967fd3a31905ef258f70ec9bffaa21f3841e3a8
1329728 <S01329728@acad.tri-c.edu>
2025-07-25 17:30:08 +0000

    Lab 4 done

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56c077077e065dd364fc26c108ea56a51d7aff4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88590bb152d59de511ad9b72abc67f611a7b2faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.VisualBasic;
 
 class Car
 {
@@ -14,6 +15,36 @@ class Car
         Console.WriteLine("Color: " + color);
         Console.WriteLine("Year: " + year);
     }
+
+    //Problem 1
+    public void Start()
+    {
+        Console.WriteLine("The car is starting.");
+
+    }
+
+    //Problem 2
+    public void Drive(int miles)
+    {
+        Console.WriteLine("The car drove" + miles + "miles.");
+
+    }
+
+    //Problem 3
+    internal string GetDescription()
+    {
+        return year + " " + color + " " + model;
+    }
+
+    //Problem 4
+    public void Repaint(string newColor)
+    {
+        color = newColor;
+        Console.WriteLine("The car has been repainted to " + newColor);
+    }
+
+
+
 }
 
 class Program
@@ -27,6 +58,13 @@ class Program
         myCar.year = 2020;
 
         myCar.Display(); // Calling the Display method
+        myCar.Start();
+        myCar.Drive(50);
+
+        string description = myCar.GetDescription();
+        Console.WriteLine("Description:" + description);
+        myCar.Repaint("red");
+        myCar.Display();
 
 
     }
